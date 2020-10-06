e53fe19a2abec9490c0da90c7a76aee36358fe5a
Juan carlos <juancarloscruz278@gmail.com>
2020-10-06 01:12:21 -0400

    Fix: Changes location for sidebar Mock Data

--- src/components/Organism/AsideOptions.jsx
+++ src/components/Organism/AsideOptions.jsx
@@ -1,51 +1,8 @@
 import React from 'react';
 import TitleIcon from '../Molecules/TitleIcon';
 import { Icon } from '@material-ui/core';
-
+import { header_options } from '../../data';
 const AsideOptions = () => {
-    const header_options = [
-        {
-            icon: 'show_chart',
-            name: 'REPORTING',
-            items: [
-                { name: 'Overview' },
-                { name: 'LeadManagerFX', has_arrow: true },
-                { name: 'CallTrackerFX', has_arrow: true },
-                { name: 'CompanyTrackerFx' },
-                { name: 'CPLdashboardFX', has_arrow: true },
-                { name: 'ContentAnalyticsFX', has_arrow: true },
-                { name: 'CustomExport' },
-            ],
-        },
-        {
-            icon: 'fingerprint',
-            name: 'MARKETING & INTELLIGENCE',
-            items: [
-                { name: 'CompetitorSpyFX', has_arrow: true },
-                { name: 'PredictionGeniusFX', has_arrow: true },
-                { name: 'LeadNurtureFX', has_arrow: true },
-                { name: 'ReviewBoostFX', has_arrow: true },
-                { name: 'PersonalizeFX', has_arrow: true },
-                { name: 'ClickBoostFX', has_arrow: true },
-                { name: 'Audience Targeting', has_beta: true },
-                { name: 'Local FX', has_arrow: true, has_beta: true },
-            ],
-        },
-        {
-            icon: 'layers',
-            name: 'CONTENT',
-            items: [
-                { name: 'ContentGeniusFX', isActive: true },
-                { name: 'Link Campaing History' },
-                { name: 'DeepLearningFX', has_beta: true },
-            ],
-        },
-        {
-            icon: 'settings',
-            name: 'CONFIGURATION',
-            items: [{ name: 'Settings' }],
-        },
-    ];
     return (
         <>
             {header_options.map(({ icon, name, items }, index) => (
---
+++ src/data.js
@@ -0,0 +1,43 @@
+export const header_options = [
+    {
+        icon: 'show_chart',
+        name: 'REPORTING',
+        items: [
+            { name: 'Overview' },
+            { name: 'LeadManagerFX', has_arrow: true },
+            { name: 'CallTrackerFX', has_arrow: true },
+            { name: 'CompanyTrackerFx' },
+            { name: 'CPLdashboardFX', has_arrow: true },
+            { name: 'ContentAnalyticsFX', has_arrow: true },
+            { name: 'CustomExport' },
+        ],
+    },
+    {
+        icon: 'fingerprint',
+        name: 'MARKETING & INTELLIGENCE',
+        items: [
+            { name: 'CompetitorSpyFX', has_arrow: true },
+            { name: 'PredictionGeniusFX', has_arrow: true },
+            { name: 'LeadNurtureFX', has_arrow: true },
+            { name: 'ReviewBoostFX', has_arrow: true },
+            { name: 'PersonalizeFX', has_arrow: true },
+            { name: 'ClickBoostFX', has_arrow: true },
+            { name: 'Audience Targeting', has_beta: true },
+            { name: 'Local FX', has_arrow: true, has_beta: true },
+        ],
+    },
+    {
+        icon: 'layers',
+        name: 'CONTENT',
+        items: [
+            { name: 'ContentGeniusFX', isActive: true },
+            { name: 'Link Campaing History' },
+            { name: 'DeepLearningFX', has_beta: true },
+        ],
+    },
+    {
+        icon: 'settings',
+        name: 'CONFIGURATION',
+        items: [{ name: 'Settings' }],
+    },
+];
